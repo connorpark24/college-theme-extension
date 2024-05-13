7ff31923c07cdf2302da68e412c33819761b35c7
Connor Park <connorpark024@gmail.com>
2024-05-13 12:10:38 -0400

    add basic highlight functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Extension "codeResourceHelper" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-extension" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "codeResourceHelper.showResources",
+    () => {
+      const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-extension!');
-	});
+      if (editor) {
+        const selection = editor.selection;
+        const text = editor.document.getText(selection);
 
-	context.subscriptions.push(disposable);
+        if (text) {
+          const resources = getResourcesForCode(text);
+          showResources(resources);
+        } else {
+          vscode.window.showInformationMessage(
+            "Please select some code to find resources."
+          );
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getResourcesForCode(code: string): string[] {
+  // Placeholder: Implement your logic here to determine resources based on the code
+  return [
+    "https://example.com/resource1",
+    "https://example.com/resource2",
+    "https://example.com/resource3",
+  ];
+}
+
+function showResources(resources: string[]): void {
+  const message = resources.join("\n");
+  vscode.window.showInformationMessage(`Resources for your code:\n${message}`);
+}
